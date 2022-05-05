6eb1d4a3f0d85becdd0728c0759bc2c583ab719e
GustavoDantasSantos <guvinidantas@gmail.com>
2022-05-04 21:09:42 -0300

    feat: Create components and imports
    Create Component Screenshot and Loading, refactor to use them in the feedbackContentStep

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
 import { ArrowLeft } from "phosphor-react";
+import { useState } from "react";
 import { feedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
     feedbackType: feedbackType;
@@ -8,7 +10,10 @@ interface FeedbackContentStepProps {
 }
 
 export const FeedbackContentStep = ({onFeedbackRestartRequested ,feedbackType}:FeedbackContentStepProps) => {
-    const feedbackTypeInfo = feedbackTypes[feedbackType]
+    const [screenshot, setScreenshot] = useState<string | null>(null);
+
+    const feedbackTypeInfo = feedbackTypes[feedbackType];
+
     return (
         <>
             <header>
@@ -34,6 +39,11 @@ export const FeedbackContentStep = ({onFeedbackRestartRequested ,feedbackType}:F
                 />
 
                 <footer className="flex gap-2 mt-2">
+                    <ScreenshotButton 
+                        screenshot={screenshot}
+                        onScreenshotTook={setScreenshot}
+                    />
+
                     <button 
                         type="submit"
                         className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
